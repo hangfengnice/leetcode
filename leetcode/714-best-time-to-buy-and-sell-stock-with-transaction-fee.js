b55c19b03f7ba6189fe102998f0896e20874124e
hangfeng <nicehangfeng@foxmail.com>
2020-10-14 13:54:36 +0800

    dynamic
    714

---
+++ leetcode/714-best-time-to-buy-and-sell-stock-with-transaction-fee.js
@@ -0,0 +1,9 @@
+var maxProfit = function (prices, fee) {
+  let cash = 0,
+    hold = -prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    cash = Math.max(cash, hold + prices[i] - fee);
+    hold = Math.max(hold, cash - prices[i]);
+  }
+  return cash;
+};
