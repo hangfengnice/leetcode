af3d358ecdbe1df0d65398fbebe452bf38c36e3b
hangfeng <nicehangfeng@foxmail.com>
2021-03-02 12:15:19 +0800

    fix

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-const buildTree = function (preorder, inorder) {
-  if (!inorder.length) return null
-  const root = new Tree
-}
+var countPrimes = function (n) {
+  let ans = 0;
+  for (let i = 2; i < n; i++) {
+    ans += isPrime(i);
+  }
+  return ans;
+  function isPrime(x) {
+    for (let i = 2; i * i <= x; i++) {
+      if (x % i == 0) return false;
+    }
+    return true;
+  }
+};
