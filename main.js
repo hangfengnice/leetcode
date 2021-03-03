be88119393776a370f8f6779f0c0853fbad49b3e
hangfeng <nicehangfeng@foxmail.com>
2021-03-03 19:24:46 +0800

    feat

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-function moveZeroes(nums) {
-  let k = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i]) {
-      if (k != i) {
-        let temp = nums[k];
-        nums[k] = nums[i];
-        nums[i] = temp;
+var checkPossibility = function(nums) {
+  let cnt = 0, n = nums.length
+  for(let i = 0; i < n - 1; i ++) {
+    const x = nums[i], y = nums[i + 1]
+    if (x > y) {
+      cnt ++
+      if (cnt > 1) return false
+
+      if (!i && y < nums[i - 1]) {
+        nums[i + 1] = x
       }
-      k++;
     }
   }
-}
+  return true
+};
