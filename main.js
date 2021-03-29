3ba297ee41f4f41dd2bc2d4ffb6a702580bf46f1
hangfeng <nicehangfeng@foxmail.com>
2021-03-29 14:01:08 +0800

    refactor

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-let M1 = 0b01
+var findErrorNums = function (nums) {
 
-let n = 0b10
-
-console.log(n >>> 1 & M1);
-console.log((n & M1) << 1);
-n = n >>> 1 & M1 | (n & M1) << 1
-
-
-console.log(n)
+};
