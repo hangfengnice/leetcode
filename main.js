931facc1096fb439cb350ba1130b5a034934ee93
hangfeng <nicehangfeng@foxmail.com>
2021-03-29 20:35:27 +0800

    feat

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 var findErrorNums = function (nums) {
 
 };
+
+
+function flatten(arr, depth = 1)  {
+  return arr.reduce((a, v) => a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1): v), [])
+}
+
+
+let arr = [[[12, 3], 4]]
+
+let res = flatten(arr)
+
+console.log([].concat(arr));
+
+console.log(res);
