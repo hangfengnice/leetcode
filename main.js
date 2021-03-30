364d21919b606ddc9cfb56a7b2c5b76203dba0cc
hangfeng <nicehangfeng@foxmail.com>
2021-03-30 22:08:46 +0800

    feat

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-var moveZeroes = function (nums) {
-  let slow = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    if (!!nums[i]) {
-      if (slow != i) {
-        let temp = nums[i];
-        nums[i] = nums[slow];
-        nums[slow] = temp;
-      }
-      slow++;
+var generate = function(numRows) {
+  let res = []
+  for(let i = 0; i < numRows; i ++) {
+    const row = new Array(i + 1).fill(1)
+    for(let j = 1; j < row.length - 1; j ++) {
+      row[j] = res[i - 1][j - 1] + res[i - 1][j]
     }
+    res.push(row)
   }
+
+  return res
 };
 
-let arr = [1, 3, 5, 0, 4];
+console.log(generate(4));
 
-let res = moveZeroes(arr);
 
-console.log(arr);
+var getRow = function (rowIndex) {
+  let row = new Array(rowIndex + 1).fill(0)
+  row[0] = 1
+  for(let i = 1; i <= rowIndex; i ++) {
+    for(let j = i; j > 0; j --) {
+      row[j] += row[j - 1]
+    }
+  }
+  return row
+}
+
+console.log(getRow(2));
