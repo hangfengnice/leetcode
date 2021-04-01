ca58c7eb57ba967167092f6b8bc45afe8caf5593
hangfeng <nicehangfeng@foxmail.com>
2021-04-01 09:04:14 +0800

    fix

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-var combine = function (n, k) {
-
+var subsets = function (nums) {
+    let res = [];
+    dfs([], 0);
+    return res;
+    function dfs(temp, start) {
+        res.push(temp.slice());
+        for (let i = start; i < nums.length; i++) {
+            temp.push(nums[i]);
+            dfs(temp, i + 1);
+            temp.pop();
+        }
+    }
 };
-
