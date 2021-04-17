af8a1cfba25de383c907ab6382df82170d7ce8c4
hangfeng <nicehangfeng@foxmail.com>
2021-04-17 10:17:22 +0800

    fix

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-let arr = [5, 3, 4, 0]
+var containsNearbyAlmostDuplicate = function (nums, t, k) {
+  let len = nums.length;
+  if (len == 0 || k <= 0 || t < 0) {
+    return false;
+  }
 
-function getSequence() {
-
-}
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (Math.abs(j - i) <= k && Math.abs(nums[j] - nums[i]) <= t) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
