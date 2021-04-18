1edeabb9dd8604a2b083c5c2f6ed0d3b9be0080f
hangfeng <nicehangfeng@foxmail.com>
2021-04-18 21:41:01 +0800

    FEAT

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-var containsNearbyAlmostDuplicate = function (nums, t, k) {
-  let len = nums.length;
-  if (len == 0 || k <= 0 || t < 0) {
-    return false;
-  }
-
-  for (let i = 0; i < len; i++) {
-    for (let j = i + 1; j < len; j++) {
-      if (Math.abs(j - i) <= k && Math.abs(nums[j] - nums[i]) <= t) {
-        return true;
+var removeDuplicates = function(nums) {
+  let l = 1
+  for(let i = 1; i < nums.length; i ++) {
+    if (nums[i] != nums[i - 1]) {
+      if (l != i) {
+        nums[l ++] = nums[i]
       }
     }
   }
-  return false;
+  return l
 };
