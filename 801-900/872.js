76238adb665636f1847c6eb30cbf3ce91bf3e11f
hangfeng <nicehangfeng@foxmail.com>
2021-05-10 09:22:00 +0800

    Create 872.js

---
+++ 801-900/872.js
@@ -0,0 +1,26 @@
+var leafSimilar = function (root1, root2) {
+  const seq1 = [];
+  if (root1) {
+    dfs(root1, seq1);
+  }
+
+  const seq2 = [];
+  if (root2) {
+    dfs(root2, seq2);
+  }
+
+  return seq1.toString() === seq2.toString();
+
+  function dfs(node, seq) {
+    if (!node.left && !node.right) {
+      seq.push(node.value);
+    } else {
+      if (node.left) {
+        dfs(node.left, seq);
+      }
+      if (node.right) {
+        dfs(node.right, seq);
+      }
+    }
+  }
+};
