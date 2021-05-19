5c0dabb5f074e39f397eafb5aac4b2aa8c23fa11
hangfeng <nicehangfeng@foxmail.com>
2021-05-19 20:00:04 +0800

    feat

---
+++ 1701-1800/1738.js
@@ -0,0 +1,16 @@
+var kthLargestValue = function (matrix, k) {
+  const m = matrix.length,
+    n = matrix[0].length;
+  const pre = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+
+  const results = [];
+  for (let i = 1; i < m + 1; i++) {
+    for (let j = 1; j < n + 1; j++) {
+      pre[i][j] =
+        pre[i - 1][j] + pre[i - 1][j - 1] + pre[i][j - 1] + matrix[i][j];
+      results.push(pre[i][j]);
+    }
+  }
+  results.sort((a, b) => b - a);
+  return results[k - 1];
+};
--- main.js
+++ main.js
@@ -1,12 +1,57 @@
-var a = {
-  [Symbol(2)]: 2,
+let names = ["iPhone X", "iPhone XS"];
+
+let colors = ["黑色", "白色"];
+
+let storages = ["64g", "256g"];
+
+let combine = function (...chunks) {
+  let res = [];
+
+  helps(0, []);
+
+  return res
+
+  function helps(chunkIndex, prev) {
+    let chunk = chunks[chunkIndex];
+
+    let isLast = chunkIndex === chunks.length - 1;
+
+    for (let val of chunk) {
+      let cur = prev.concat(val);
+      if (isLast) {
+        res.push(cur);
+      } else {
+        helps(chunkIndex + 1, cur);
+      }
+    }
+  }
+};
+
+function combination(n, k) {
+  let res = []
+  dfs(1, [])
+
+  return res
+
+  function dfs(start, arr) {
+    if (arr.length === k) {
+      return res.push(arr.slice())
+    }
+
+    for(let i = start; i <= n; i ++) {
+      arr.push(i)
+      dfs(i + 1, arr)
+      arr.pop()
+    }
+  }
 }
+console.log(combination(4, 2));
 
-Object.defineProperty(a, 'b', {
-  enumerable: false,
-})
+let a = [1, 2]
 
-let res1 = Reflect.ownKeys(a) // [Symbol(2), 'b']
-let res2 = Object.keys(a)
+let b = a.concat(2)
+b.push(2)
 
-console.log(res1, res2);
+console.log(a, b, 'a, b')
+
+
