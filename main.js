fc15b7388079c98e9c89255f75e9ac1a43763070
hangfeng <nicehangfeng@foxmail.com>
2021-05-24 09:07:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-async function foo() {
-  console.log("foo");
+var o = {
+  valueOf : () => {console.log("valueOf"); return {}},
+  toString : () => {console.log("toString"); return {}}
 }
-async function bar() {
-  console.log("bar start");
-  await foo();
-  console.log("bar end");
-}
-console.log("script start");
-setTimeout(function () {
-  console.log("setTimeout");
-}, 0);
-bar();
-new Promise(function (resolve) {
-  console.log("promise executor");
-  resolve();
-}).then(function () {
-  console.log("promise then");
-});
-console.log("script end");
+
+o + ""
