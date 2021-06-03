f7766a1d4b52743abe396932a25c02eb845d4c25
hangfeng <nicehangfeng@foxmail.com>
2021-06-03 09:29:54 +0800

    fix

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-var subarraySum = function (nums, k) {
-  const map = new Map();
-  map.set(0, 1);
-  let count = 0,
-    pre = 0;
-  for (const x of nums) {
-    pre += x;
-    if (map.has(pre - k)) {
-      count += map.get(pre - k);
-    }
+let maxW = Number.MIN_VALUE
 
-    if (map.has(pre)) {
-      map.set(pre, map.get(pre) + 1);
-    } else {
-      map.set(pre, 1);
-    }
+let weight = [2, 2, 4, 6, 3]
+let n = 5
+let w = 9
+
+function f(i, cw) {
+  if (cw === w || i === n) {
+
   }
-  return count;
-};
-
-let res = subarraySum([1, 1, 1], 2)
-
-console.log(res, 'res');
+}
