33f537662d2d626d3f465cff0f2a47f40b37f33f
hangfeng <nicehangfeng@foxmail.com>
2021-06-03 17:43:06 +0800

    update

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-let maxW = Number.MIN_VALUE
+var isPalindrome = function (head) {
+  if (!head || !head.next) return head;
 
-let weight = [2, 2, 4, 6, 3]
-let n = 5
-let w = 9
+  let vals = [];
 
-function f(i, cw) {
-  if (cw === w || i === n) {
-
+  while (head) {
+    vals.push(head.val);
+    head = head.next;
   }
-}
+
+  for (let i = 0, j = vals.length - 1; i < j; i++, j--) {
+    if (vals[i] != vals[j]) {
+      return false;
+    }
+  }
+};
+
+var isPalindrome = function (head) {
+  let frontPointer = head;
+  return recursivelyCheck(head);
+
+  function recursivelyCheck(currenNode) {
+    if (currenNode !== null) {
+      if (!recursivelyCheck(currenNode.next)) {
+        return false;
+      }
+      if (currenNode.val !== frontPointer.val) {
+        return false;
+      }
+      frontPointer = frontPointer.next;
+    }
+    return true;
+  }
+};
+
+[1, 2, 3] 4
