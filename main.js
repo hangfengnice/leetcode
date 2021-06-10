385dc300c1e4e129cdc015080bac08e0d6502984
hangfeng <nicehangfeng@foxmail.com>
2021-06-10 10:25:01 +0800

    feat

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-var findErrorNums = function (nums) {
-  debugger
-  let dup = -1, missing = 1
-  for(let num of nums) {
-    if (nums[Math.abs(num) - 1] < 0) {
-      dup = Math.abs(num)
-    } else {
-      nums[Math.abs(num) - 1] *= -1
-    }
-  }
+var profitableSchemes = function (n, minProfit, group, profit) {
+  const len = group.length,
+    MOD = 1e9 + 7;
 
-  for(let i = 1; i < nums.length; i ++) {
-    if (nums[i] > 0) {
-      missing = i + 1
+  const dp = new Array(len + 1)
+    .fill(0)
+    .map(() =>
+      new Array(n + 1).fill(0).map(() => new Array(minProfit + 1).fill(0))
+    );
+  dp[0][0][0] = 1;
+
+  for (let i = 1; i <= len; i++) {
+    const members = group[i - 1],
+      earn = profit[i - 1];
+
+    for (let j = 0; j <= n; j++) {
+      for (let k = 0; k <= minProfit; k++) {
+        if (j < members) {
+          dp[i][j][k] = dp[i - 1][j][k];
+        } else {
+          dp[i][j][k] =
+            (dp[i - 1][j][k] + dp[i - 1][j - members][Math.max(0, k - earn)]) %
+            MOD;
+        }
+      }
     }
   }
-  return [dup, missing]
+  let sum = 0;
+  for (let j = 0; j <= n; j++) {
+    sum = (sum + dp[len][j][minProfit]) % MOD;
+  }
+  return sum;
 };
-
-let res = findErrorNums([1, 1])
-
-console.log(res, 'res');
