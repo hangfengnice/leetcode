bb0dc8caee432a5347bcfc30924496e33969ad0c
hangfeng <nicehangfeng@foxmail.com>
2021-06-21 22:04:14 +0800

    review

--- 301-400/396-rotate-function.js
+++ 301-400/396-rotate-function.js
@@ -1,3 +1,11 @@
+// 推导过程：
+// （1）F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-2) * Bk[n-2] + (n-1) * Bk[n-1]
+// （2）F(k+1) = 0 * Bk[n-1] + 1 * Bk[0] + 2 * Bk[2] + ... + (n-1) * Bk[n-2]
+// （2）-（1）得：F(k+1) - F(k) = (Bk[0] + Bk[1] + ... + Bk[n-2]) - (n-1)*Bk[n-1]
+// 可得：F(k+1) - F(k) = (Bk[0] + Bk[1] + ... + Bk[n-2] + Bk[n-1]) - n*Bk[n-1]
+// 令S=Sum{Bk}
+// 有：F(k+1) = F(k) + S - n * Bk[n-1]
+
 var maxRotateFunction = function (A) {
   let n = A.length;
   let s = 0;
--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-class Person {
-
-  static getP() {
-    console.log(this.p, 'hello ');
-    return this.p
+var maxRotateFunction = function (nums) {
+  let n = nums.length;
+  let s = 0;
+  let t = 0;
+  for (let i = 0; i < n; i++) {
+    s += A[i];
+    t += i * A[i];
   }
-}
 
-console.log(Person.getP());
+  let res = t;
+  for (let i = n - 1; i > 0; i--) {
+    t += s - n * nums[i];
+    res = Math.max(res, t);
+  }
+  return res;
+};
