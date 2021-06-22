4e96a823f40dbc07239e870f4c69c053f0e6caca
hangfeng <nicehangfeng@foxmail.com>
2021-06-22 11:29:15 +0800

    feat
    permutation
    next permutation

--- 1-100/31-next-permutation.js
+++ 1-100/31-next-permutation.js
@@ -1,32 +1,35 @@
+// 此题的题解 能够影响到 剑指 offer 38 字符串的排列
+
 var nextPermutation = function (nums) {
-  let i = nums.length - 2
-  while(i >= 0 && nums[i] >= nums[i + 1]) {
-    i --
+  let i = nums.length - 2;
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
+    i--;
   }
   if (i >= 0) {
-    let j = nums.length - 1
-    while(j >= 0 && nums[i] >= nums[j]) {
-      j --
+    let j = nums.length - 1;
+    while (j >= 0 && nums[i] >= nums[j]) {
+      j--;
     }
-    swap(nums, i, j)
+    swap(nums, i, j);
   }
-  reverse(nums, i + 1)
-  return nums
+  reverse(nums, i + 1);
+  return nums;
   function swap(nums, i, j) {
-    let temp = nums[i]
-    nums[i] = nums[j]
-    nums[j] = temp
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
   }
   function reverse(nums, start) {
-    let left = start, right = nums.length - 1
-    while(left < right) {
-      swap(nums, left, right)
-      left ++
-      right --
+    let left = start,
+      right = nums.length - 1;
+    while (left < right) {
+      swap(nums, left, right);
+      left++;
+      right--;
     }
   }
-}
+};
 
-let res = nextPermutation([1, 5, 1])
+let res = nextPermutation([1, 5, 1]);
 
 console.log(res);
--- main.js
+++ main.js
@@ -1,16 +1,52 @@
-var maxRotateFunction = function (nums) {
-  let n = nums.length;
-  let s = 0;
-  let t = 0;
-  for (let i = 0; i < n; i++) {
-    s += A[i];
-    t += i * A[i];
+var permutation = function (s) {
+  const ret = []
+  const n = s.length
+
+  const arr = Array.from(s).sort()
+
+  do {
+    ret.push(arr.join(''))
+  } while(nextPermutation(arr))
+
+  return ret
+
+
+  function nextPermutation(arr) {
+    let i = n - 2
+
+    while(i >= 0 && arr[i] >= arr[i + 1]) {
+      i --
+    }
+    if (i < 0) {
+      return false
+    }
+
+    let j = n - 1
+    while(j >= 0 && arr[i] >= arr[j]) {
+      j --
+    }
+    swap(arr, i, j)
+    reverse(arr, i + 1)
+    return true
   }
 
-  let res = t;
-  for (let i = n - 1; i > 0; i--) {
-    t += s - n * nums[i];
-    res = Math.max(res, t);
+  function swap(arr, i, j) {
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
   }
-  return res;
+
+  function reverse(arr, start) {
+    let l = start, r = n - 1
+    while(l < r) {
+      swap(arr, l, r)
+      l ++
+      r --
+    }
+  }
 };
+
+
+let res = permutation('aab')
+
+console.log(res, 'res');
