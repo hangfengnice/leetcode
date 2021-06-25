28edc7cc791c88d377fd9c24b7e70a44bb1afa26
hangfeng <nicehangfeng@foxmail.com>
2021-06-25 14:31:37 +0800

    update

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-var addTwoNumbers = function (l1, l2) {
-  let stack1 = [];
-  let stack2 = [];
+var exclusiveTime = function (n, logs) {
+  let stack = [];
+  let res = new Array(n).fill(0);
 
-  while (l1) {
-    stack1.push(l1.val);
-    l1 = l1.next;
+  let s = logs[0].split(":");
+
+  stack.push(parseInt(s[0]));
+
+  let i = 1
+  let prev = parseInt(s[2]);
+
+  while (i < logs.length) {
+    s = logs[i].split(":");
+    if (s[1] === "start") {
+      if (stack.length) {
+        res[stack[stack.length - 1]] += parseInt(s[2]) - prev;
+      }
+      stack.push(parseInt(s[0]));
+      prev = parseInt(s[2]);
+    } else {
+      let index = stack[stack.length - 1]
+      let result = parseInt(s[2]) - prev + 1;
+      let val = res[index]
+      res[index] = val + result
+      stack.pop();
+      prev = parseInt(s[2]) + 1;
+    }
+    i++;
   }
-  while (l2) {
-    stack2.push(l2.val);
-    l2 = l2.next;
-  }
-
-  let carry = 0;
-  let ans = null;
-  while (stack1.length || stack2.length || carry) {
-    let a = stack1.length ? stack1.pop() : 0;
-    let b = stack2.length ? stack2.pop() : 0;
-    let cur = a + b + carry;
-    carry = ~~(cur / 10);
-    cur = cur % 10;
-
-    let curNode = new ListNode(cur);
-    curNode.next = ans;
-    ans = curNode;
-  }
-
-  return ans;
+  return res;
 };
+
+let res = exclusiveTime(
+  1,
+["0:start:0","0:start:1","0:start:2","0:end:3","0:end:4","0:end:5"]
+);
+
+console.log(res, "res");
