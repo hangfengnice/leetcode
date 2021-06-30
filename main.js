3f2db26a000b9dbcb14dfef977e4b76e8f06539a
hangfeng <nicehangfeng@foxmail.com>
2021-06-30 20:32:00 +0800

    fix

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-var longestValidParentheses = function (s) {
-  let maxans = 0
-  let stack = []
-  stack.push(-1)
 
-  for(let i = 0; i < s.length; i ++) {
-    if (s[i] === '(' ) {
-      stack.push(i)
-    } else {
-      stack.pop()
-      if (!stack.length) {
-        stack.push(i)
-      } else {
-        maxans = Math.max(maxans, i - stack[stack.length - 1])
-      }
+const person = {
+  name: "Lydia Hallie",
+  age: 21,
+  *[Symbol.iterator]() {
+    console.log(this, 'this');
+    for(let x in this) {
+      yield* this[x]
     }
   }
 }
+
+console.log([...person]);
+
