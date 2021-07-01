51b3cac164d42326a3238dc75d2a4e1412d35bbf
hangfeng <nicehangfeng@foxmail.com>
2021-07-01 09:30:07 +0800

    feat

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
+var numWays = function (n, relation, k) {
+  const edge = new Array(n).fill(0).map(() => [])
 
-const person = {
-  name: "Lydia Hallie",
-  age: 21,
-  *[Symbol.iterator]() {
-    console.log(this, 'this');
-    for(let x in this) {
-      yield* this[x]
+  for(let [src, dst] of relation) {
+    edge[src].push(dst)
+  }
+
+  let steps = 0
+  const queue = [0]
+
+  while(queue.length && steps < k) {
+    steps ++
+    const size = queue.length
+
+    for(let i = 0; i < size; i ++) {
+      const index = queue.shift()
+      let list = edge[index]
+      for(const cur of list) {
+        queue.push(cur)
+      }
     }
   }
-}
+  let ways = 0
+  if (steps === k) {
+    while(queue.length) {
+      if (queue.shift() === n - 1) {
+        ways ++
+      }
+    }
+  }
+  return ways
+};
 
-console.log([...person]);
 
+let res = numWays(n = 5, relation = [[0,2],[2,1],[3,4],[2,3],[1,4],[2,0],[0,4]], k = 3)
+
+console.log(res, 'res');
