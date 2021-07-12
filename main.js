ab71c9e2db51f06ec926034333a59463707f8f0b
hangfeng <nicehangfeng@foxmail.com>
2021-07-12 12:28:53 +0800

    H 指数

--- main.js
+++ main.js
@@ -1,64 +1,22 @@
-var countOfAtoms = function (formula) {
-  let i = 0;
-  const n = formula.length;
-  const stack = [new Map()];
-
-  while (i < n) {
-    const ch = formula[i];
-    if (ch === "(") {
-      i++;
-      stack.unshift(new Map());
-    } else if (ch === ")") {
-      i++;
-      const num = parseNum();
-      const popMap = stack.shift();
-      const topMap = stack[0];
-      for (const [atom, v] of popMap.entries()) {
-        topMap.set(atom, (topMap.get(atom) || 0) + v * num);
-      }
+var majorityElement = function (nums) {
+  let candidate = -1;
+  let count = 0;
+  for (const num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    if (num === candidate) {
+      count++;
     } else {
-      const atom = parseAtom();
-      const num = parseNum();
-      const topMap = stack[0];
-      topMap.set(atom, (topMap.get(atom) || 0) + num);
+      count--;
     }
   }
-  let map = stack.pop();
-  map = Array.from(map);
-
-  map.sort();
-
-  const sb = [];
-  for (const [atom, count] of map) {
-    sb.push(atom);
-    if (count > 1) {
-      sb.push(count);
+  count = 0;
+  let length = nums.length;
+  for (const num of nums) {
+    if (num === candidate) {
+      count++;
     }
   }
-  return sb.join("");
-
-  function parseAtom() {
-    const sb = [];
-
-    sb.push(formula[i++]);
-    while (i < n && formula[i] >= "a" && formula[i] <= "z") {
-      sb.push(formula[i++]);
-    }
-    return sb.join("");
-  }
-
-  function parseNum() {
-    if (i === n || Number.isNaN(Number(formula[i]))) {
-      return 1;
-    }
-    let num = 0;
-    while (i < n && !Number.isNaN(Number(formula[i]))) {
-      num = formula[i ++] - 0 + num * 10;
-    }
-    return num;
-  }
+  return count * 2 > length ? candidate : -1;
 };
-
-let res = countOfAtoms('Mg(OH)2')
-
-console.log(res)
