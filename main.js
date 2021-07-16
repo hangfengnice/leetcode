8a785dbe2c2af3271fccf23f31ba0bf4268a8dcc
hangfeng <nicehangfeng@foxmail.com>
2021-07-16 12:38:42 +0800

    Create main.vue

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-var hIndex = function (citations) {
-  let n = citations.length;
-  const left = 0,
-    right = n - 1;
-  while (left <= right) {
-    const mid = ~~(right - left) / 2 + left;
-    if (citations[mid] >= n - mid) {
-      right = mid - 1;
+var maximumElementAfterDecrementingAndRearranging = function (arr) {
+  const n = arr.length;
+  arr.sort((a, b) => a - b);
+
+  arr[0] = 1;
+  for (let i = 1; i < n; i++) {
+    arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+  }
+  return arr[n - 1];
+};
+
+var maximumElementAfterDecrementingAndRearranging = function (arr) {
+  const n = arr.length;
+  const cnt = new Array(n + 1).fill(0);
+
+  for (const v of arr) {
+    ++cnt[Math.min(v, n)];
+  }
+
+  let miss = 0;
+  for (let i = 1; i <= n; i++) {
+    if (cnt[i] === 0) {
+      miss++;
     } else {
-      left = mid + 1;
+      miss -= Math.min(cnt[i] - 1, miss);
     }
   }
-  return n - left;
+  return n - miss;
 };
