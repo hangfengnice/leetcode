dfc60663dd627692697f47e20e23cd808cb410f4
hangfeng <nicehangfeng@foxmail.com>
2021-07-19 19:45:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,5 @@
-var search = function (nums, target) {
-  let ans = 0;
-  const leftIdx = binarySearch(nums, target, true);
-  const rightIdx = binarySearch(nums, target, false) - 1;
-
-  if (
-    leftIdx <= rightIdx &&
-    rightIdx < nums.length &&
-    nums[leftIdx] === target &&
-    nums[rightIdx] === target
-  ) {
-    ans = rightIdx - leftIdx + 1;
-  }
-
-  return ans;
-
-  function binarySearch(nums, target, lower) {
-    let left = 0,
-      right = nums.length - 1,
-      ans = nums.length;
-
-    while (left <= right) {
-      const mid = Math.floor((right + left) / 2);
-
-      if (nums[mid] > target || (lower && nums[mid] >= target)) {
-        right = mid - 1;
-        ans = mid;
-      } else {
-        left = mid + 1;
-      }
-    }
-    return ans;
-  }
-};
-
-let res = search([5,7,7,8,8,10], 8)
-
-console.log(res, 'res')
+['7z', 'rar', 'zip', 'apz', 'ar', 'bz', 'car', 'dar', 'cpgz', 'f', 'ha', 'hbc', 'hbc2', 'hbe', 'hpk', 'hyp'].some(function (append) {
+  let reg = new RegExp('\\.' + append + '$')
+  console.log(reg.source, 'reg.source');
+  return reg.test("吴航锋-前端-社招-杭州.7z")
+})
