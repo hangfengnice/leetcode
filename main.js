6ba32c793a34d1cf2ecaec789e1f5424af04e32a
hangfeng <nicehangfeng@foxmail.com>
2021-07-23 12:30:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-['7z', 'rar', 'zip', 'apz', 'ar', 'bz', 'car', 'dar', 'cpgz', 'f', 'ha', 'hbc', 'hbc2', 'hbe', 'hpk', 'hyp'].some(function (append) {
-  let reg = new RegExp('\\.' + append + '$')
-  console.log(reg.source, 'reg.source');
-  return reg.test("吴航锋-前端-社招-杭州.7z")
-})
+var copyRandomList = function (head, cachedNode = new Map()) {
+  if (head === null) {
+    return null;
+  }
+  for (let node = head; node !== null; node = node.next.next) {
+    const nodeNew = new Node(node.val, node.next, null);
+    node.next = nodeNew;
+  }
 
+  for (let node = head; node !== null; node = node.next.next) {
+    const nodeNew = node.next;
+    nodeNew.random = node.random !== null ? node.random.next : null;
+  }
+  const headNew = head.next;
 
-thunder://QUFodHRwczovL3d3dy5tbXh6eGwxLmNvbS9jb21tb24vZ2MvMjAxOTA0MDcvdGhTbk1obkgubXA0Wlo=
+  for (let node = head; node !== null; node = node.next) {
+    const nodeNew = node.next;
+    node.next = node.next.next;
+    nodeNew.next = nodeNew.next !== null ? nodeNew.next.next : null;
+  }
 
-thunder://QUFodHRwczovL3d3dy5tbXh6eGwxLmNvbS9jb21tb24vZ2MvMjAxOTA0MTAvdWNwS3BoS3YubXA0Wlo=
-
-thunder://QUFodHRwczovL3d3dy5tbXh6eGwxLmNvbS9jb21tb24vZ2MvMjAxOTA0MjAvRnJQanN3d1UubXA0Wlo=
-
-thunder://QUFodHRwczovL3d3dy5tbXh6eGwxLmNvbS9jb21tb24vZ2MvMjAxOTA0MjUvd2FFQm5tVGUubXA0Wlo=
-
-thunder://QUFodHRwczovL3d3dy5tbXh6eGwxLmNvbS9jb21tb24vZ2MvMjAxOTA0MjUvTGhUVFhkTXAubXA0Wlo=
-
-// 吃
-thunder://QUFodHRwczovL3d3dy5tbXh6eGwxLmNvbS9jb21tb24vZ2MvMjAxOTA0MjUvbktzVkVXWUgubXA0Wlo=
-
-394
-
-尤物馆 248期
+  return headNew;
+};
