13e7bec27b48a351e44ed42c2a50b2164201c763
hangfeng <nicehangfeng@foxmail.com>
2021-08-11 12:11:06 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+var numberOfArithmeticSlices = function (nums) {
+  let n = nums.length;
+
+  if (n === 1) {
+    return 0;
+  }
+
+  let d = nums[0] - nums[1],
+    t = 0;
+  let ans = 0;
+
+  for (let i = 2; i < n; i++) {
+    if (nums[i - 1] - nums[i] === d) {
+      t++;
+    } else {
+      d = nums[i - 1] - nums[i];
+      t = 0;
+    }
+    ans += t;
+  }
+  return ans;
+};
+let res = numberOfArithmeticSlices([1, 2, 3, 4, 5]);
+console.log(res, "res");
