b31d9fdc029e2b8c624d776eb94637ca3d3939a1
hangfeng <nicehangfeng@foxmail.com>
2021-09-27 10:13:57 +0800

    create

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-var reverseStr = function (s, k) {
-  const n = s.length;
-  const arr = Array.from(s);
-  for (let i = 0; i < n; i += 2 * k) {
-    reverse(arr, i, Math.min(i + k, n) - 1);
-  }
-  return arr.join("");
+var number = /^(\+|-|)(\d+(\.\d*)?|\.\d+)([eE](\+|-|)\d+)?$/;
 
-  function reverse(arr, l, r) {
-    while (l < r) {
-      let temp = arr[l];
-      arr[l] = arr[r];
-      arr[r] = temp;
-      l++;
-      r--;
-    }
-  }
-};
-
-let res = reverseStr("abcdefg", 2);
-console.log(res, "res");
+console.log(number.test('+-1'));
