d8fe542c58674cddeacc5db60c11a81505ca45b0
hangfeng <nicehangfeng@foxmail.com>
2021-10-12 09:18:40 +0800

    魔塔

---
+++ 101-200/174.js
@@ -0,0 +1,16 @@
+var calculateMinimumHP = function (dungeon) {
+  let n = dungeon.length,
+    m = dungeon[0].length;
+  let dp = new Array(n + 1).fill(0).map(() => new Array(m + 1).fill(Infinity));
+
+  dp[n][m - 1] = dp[n - 1][m] = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = m - 1; j >= 0; j--) {
+      let minn = Math.min(dp[i + 1][j], dp[i][j + 1]);
+      dp[i][j] = Math.max(minn - dungeon[i][j], 1);
+    }
+  }
+  return dp[0][0];
+};
+
+calculateMinimumHP();
---
+++ main.js
@@ -0,0 +1,23 @@
+var minPathSum = function (grid) {
+  let m = grid.length;
+  let n = grid[0].length;
+  let dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  dp[0][0] = grid[0][0];
+  for (let i = 1; i < m; i++) {
+    dp[0][i] = dp[0][i - 1] + grid[0][i];
+  }
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = dp[i - 1][0] + grid[i][0];
+  }
+  console.log(dp, 'dp');
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+    }
+  }
+  return dp[m - 1][n - 1];
+};
+
+let res = minPathSum([[1,3,1],[1,5,1],[4,2,1]])
+console.log(res, 'res');
