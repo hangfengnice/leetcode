9b8e823220c2c88c389d9ecb5d4abb04138a9cb4
hangfeng <nicehangfeng@foxmail.com>
2021-11-05 09:08:50 +0800

    main

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
-var minMeetingRooms = function (
-  intervals,
-  s = [],
-  e = [],
-  l = 0,
-  r = 0,
-  n = 0
-) {
-  intervals.forEach((v) => (s.push(v[0]), e.push(v[1])));
-  s.sort((a, b) => a - b);
-  e.sort((a, b) => a - b);
-  console.log(s, e);
-  while (l < s.length) e[r] > s[l] ? n++ : r++, l++;
-  return n;
-};
+// const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
+// console.log('class="some-class"'.match(attribute))  // 测试双引号
+// console.log("class='some-class'".match(attribute))  // 测试单引号
+// console.log('class=some-class'.match(attribute))  // 测试无引号
+// console.log('disabled'.match(attribute))
 
-let res = minMeetingRooms([
-  [2, 3],
-  [1, 2],
-  [1, 4],
-]);
-console.log(res, "res");
+let ncname = `[a-zA-Z_][\-\.0-9_a-zA-Z]*`
+// console.log(ncname);
+console.log(new RegExp(ncname).test('a.'));
