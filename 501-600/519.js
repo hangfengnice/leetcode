057315a178176b07f1de03f63a2f55309293992e
hangfeng <nicehangfeng@foxmail.com>
2021-11-29 11:12:30 +0800

    feat

---
+++ 501-600/519.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ */
+var Solution = function (m, n) {
+  this.m = m;
+  this.n = n;
+  this.total = m * n;
+  this.map = new Map();
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.flip = function () {
+  const x = Math.floor(Math.random() * this.total);
+  this.total--;
+  const idx = this.map.get(x) || x;
+  this.map.set(x, this.map.get(this.total) || this.total);
+  return [Math.floor(idx / this.n), idx % this.n];
+};
+
+/**
+ * @return {void}
+ */
+Solution.prototype.reset = function () {
+  this.total = this.m * this.n;
+  this.map.clear();
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(m, n)
+ * var param_1 = obj.flip()
+ * obj.reset()
+ */
