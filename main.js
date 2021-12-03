f843e1402c1d5f0286f7daf608493d7d75d72db7
hangfeng <nicehangfeng@foxmail.com>
2021-12-03 18:00:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,29 @@
-function cloneDeep(target, map = new Map()) {
-
-  if (typeof target !== 'object') return target
-
-  if (map.get(target)) {
-    return map.get(target)
+var largestSumAfterKNegations = function (nums, k) {
+  const freq = new Map()
+  for(num of nums) {
+    freq.set(num, (freq.get(num) || 0) + 1)
   }
-  const temp = Array.isArray(target) ? [] : {}
 
-  map.set(target, temp)
-  for(const key in target) {
-    temp[key] =  cloneDeep(target[key], map)
+  let ans = nums.reduce((prev, next) => prev + next, 0)
+  for(let i = -100; i < 0; i ++) {
+    if (freq.has(i)) {
+      const ops = Math.min(freq.get(i), k)
+      ans += (-i) * ops * 2
+      freq.set(i, freq.get(i) - ops)
+      freq.set(-i, (freq.get(-1) || 0) + ops)
+      k -= ops
+      if (k === 0) {
+         break
+      }
+    }
   }
-  return temp
+  if (k > 0 && k % 2 === 1 && !freq.has(0)) {
+    for(let i = 1; i <= 100; i ++) {
+      if (freq.has(i)) {
+        ans -= i * 2
+        break
+      }
+    }
+  }
+  return ans
 }
-
-// // let arr = [1, 2, 3, 4]
-
-// // for(let key in arr) {
-// //   console.log(key, 'key')
-// // }
-
-
-// let arrow = () => {console.log('yes')}
-
-// let other = eval(arrow.toString())
-// other()
-
-// let s = Symbol('1')
-
-
-
-// let o = Object(Symbol.prototype.valueOf.call(s))
-
-// console.log(o, o === s, 'o')
-
-// console.log(Object(s), s)
-
-let obj = {
-  name: 1
-}
-
-let obj1 = Object(obj)
-
-console.log(obj1 === obj, 'obj')
-
-let s = Symbol(1)
-
-
-let s2 = Object(s)
-
-console.log(s === s2)
