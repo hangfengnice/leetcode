621e1e1399aad25c79dfcd84f3619cc26e3f3ecc
hangfeng <nicehangfeng@foxmail.com>
2022-04-20 14:40:02 +0800

    fix

--- main.js
+++ main.js
@@ -1,39 +1,11 @@
-// let obj = {
-//   name: "1",
-//   age: 18,
-//   sex: "man",
-// }; // =》 { sex: 'man' }
+let deps = []
+let newDeps = [1, 2]
+let depIds = new Set()
+let newDepIds = new Set()
 
-// function omit(source, keys) {
-//   let data = [];
-// for(let i=0;i<=keys.length;i++){
-//   if(source.indexOf(keys[i]) != -1){
-//     data.push(keys[i])
-//   }
-// }
-// return data
-// }
 
-// let res = omit(obj, ["name", "age"]);
-// console.log(res, 'res')
+deps = newDeps
 
-let arr = ['a',  'abc', false, null, undefined, 1, 0]
-function filter(arr) {
-  return arr.filter(Boolean)
-    // let flaseArr = []
-    // let trueArr = []
-    // for(let i = 0; i<=arr.length;i++){
+newDeps.length = 0
 
-    //   if(arr[i] !== '' && arr[i] !== false && arr[i] !== 0){
-    //     trueArr.push(arr[i])
-    //   }else{
-    //     flaseArr.push(arr[i])
-    //   }
-    // }
-    // return trueArr
-}
-
-let res = filter(arr)
-
-console.log(res, 'res')
-
+console.log(deps, 'deps');
