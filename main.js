b6509656f2d22b33c614ef994d8dfa2168f5f3d2
hangfeng <nicehangfeng@foxmail.com>
2024-04-01 16:44:26 +0800

    main

--- main.js
+++ main.js
@@ -1,34 +1,25 @@
-var sellingWood = function (m, n, prices) {
-  const value = new Map()
-
-  const meno = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(-1))
-
-  for (const price of prices) {
-    value.set(pairHash(price[0], price[1]), price[2])
+var isValid = function (s) {
+  let map = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
   }
 
-  function pairHash(x, y) {
-    return (x << 16) ^ y
-  }
-  return dfs(m, n)
+  let stack = []
 
-  function dfs(x, y) {
-    if (meno[x][y] != -1) {
-      return meno[x][y]
-    }
+  for (let ch of s) {
+    debugger
+    let top = stack[stack.length - 1]
 
-    let ret = value.has(pairHash(x, y)) ? value.get(pairHash(x, y)) : 0
-    if (x > 1) {
-      for (let i = 1; i < x; i++) {
-        ret = Math.max(ret, dfs(i, y) + dfs(x - i, y))
-      }
+    if (top && map[ch] == top) {
+      stack.pop()
+    } else {
+      stack.push(ch)
     }
-    if (y > 1) {
-      for (let j = 1; j < y; j++) {
-        ret = Math.max(ret, dfs(x, j) + dfs(x, y - j))
-      }
-    }
-    memo[x][y] = ret
-    return ret
   }
+
+  console.log(stack, 'stack')
+  return stack.length == 0
 }
+
+isValid('()')
