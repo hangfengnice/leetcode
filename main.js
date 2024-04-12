4ca07bd3ad7ffb32be8ea500c0ee2e05d7128015
hangfeng <nicehangfeng@foxmail.com>
2024-04-12 16:58:34 +0800

    temp

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
-var minDistance = function (word1, word2) {
-  let n = word1.length
+var pathsWithMaxScore = function (board) {
+  let row = board.length
 
-  let m = word2.length
+  if (row === 0) return [0, 0]
 
-  if (n * m === 0) {
-    return n + m
-  }
+  let mod = 1e9 + 7
 
-  let D = new Array(n + 1).fill(0).map(() => new Array(m + 1).fill(0))
+  let col = board[0].length
 
-  for (let i = 0; i < n + 1; i++) {
-    D[i][0] = i
-  }
+  let dpscore = new Array(row + 1).fill(0).map(() => new Array(col + 1).fill(0))
+  let dppath = new Array(row + 1).fill(0).map(() => new Array(col + 1).fill(0))
 
-  for (let i = 0; i < m + 1; i++) {
-    D[0][i] = i
-  }
+  dppath[row - 1][col - 1] = 1
 
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= m; j++) {
-      let left = D[i - 1][j] + 1
+  for (let i = row - 1; i >= 0; i--) {
+    for (let j = col - 1; j >= 0; j--) {
+      let ch = board[i][j]
 
-      let right = D[i][j - 1] + 1
+      if (
+        ch !== 'X' &&
+        (dppath[i + 1][j] || dppath[i][j + 1] || dppath[i + 1][j + 1])
+      ) {
+        let maxscore = Math.max(
+          dpscore[i + 1][j],
+          dpscore[i][j + 1],
+          dpscore[i + 1][j + 1]
+        )
+        if (ch !== 'E') {
+          dpscore[i][j] = maxscore + (ch - 0)
+        } else {
+          dpscore[i][j] = maxscore
+        }
 
-      let left_down = D[i - 1][j - 1]
-
-      if (word1[i - 1] !== word2[j - 1]) {
-        left_down + 1
+        if (maxscore === dpscore[i + 1][j]) {
+          dppath[i][j] = (dppath[i][j] + dppath[i + 1][j]) % mod
+        }
+        if (maxscore === dpscore[i][j + 1]) {
+          dppath[i][j] = (dppath[i][j] + dppath[i][j + 1]) % mod
+        }
+        if (maxscore === dpscore[i + 1][j + 1]) {
+          dppath[i][j] = (dppath[i][j] + dppath[i + 1][j + 1]) % mod
+        }
       }
-      D[i][j] = Math.min(left, Math.min(right, left_down))
     }
   }
-  return D[i][j]
+
+
+  return [maxscore, dppath[0][0]]
 }
+
+pathsWithMaxScore(['E23', '2X2', '12S'])
